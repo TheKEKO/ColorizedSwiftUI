d8d4e54decb03a5b7df73dc8e818a106d84d9e1b
Aleksandr F <a7214370@gmail.com>
2022-05-16 15:41:10 +0300

    Initial Commit

---
+++ SwiftUIColorized/SwiftUIColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedApp.swift
+//  SwiftUIColorized
+//
+//  Created by Aleksandr F. on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
