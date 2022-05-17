edbe0d151a27d12a120fb25fc07b552faae147fa
Aleksandr F <a7214370@gmail.com>
2022-05-17 15:44:52 +0300

    Setup ContentView

--- SwiftUIColorized/ContentView.swift
+++ SwiftUIColorized/ContentView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-        var body: some View {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        }
-    }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+    @State private var red: Double = 20
+    @State private var green: Double = 60
+    @State private var blue: Double = 86
+    
+    var body: some View {
+        ZStack {
+            Color(#colorLiteral(red: 0.17, green: 0.41, blue: 0.74, alpha: 1))
+                .ignoresSafeArea()
+            VStack {
+                ColorView(red: red, green: green, blue: blue)
+                
+                SliderView(sliderValue: $red, textColor: .red)
+                SliderView(sliderValue: $green, textColor: .green)
+                SliderView(sliderValue: $blue, textColor: .blue)
+                
+                Spacer()
+            }.padding()
         }
     }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
